99730807e4d40c061980dd88c4f3b22f109f73b5
Jonathan Suchland <jonathansuchland@gmail.com>
2011-12-10 13:12:37 -0600

    Configuring unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,13 @@
+worker_processes 2
+working_directory "/apps/Hoot"
+
+preload_app true
+timeout 2
+
+listen "/var/run/unicorn/hoot/socket"
+pid "/var/run/unicorn/hoot/pid"
+
+stderr_path "/var/log/unicorn/hoot/error.log"
+stdout_path "/var/log/unicorn/hoot/access.log"
+
+user www-data www-data
