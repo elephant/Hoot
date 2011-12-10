03c9a477b88f4e37483edf8dc443c5c8cd829f68
Jonathan Suchland <jonathansuchland@gmail.com>
2011-12-10 13:20:54 -0600

    Fixed user syntax
    Properly set the user/group for the unicorn workers.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,6 @@ worker_processes 2
 working_directory "/apps/Hoot"
 
 preload_app true
-timeout 2
 timeout 3
 
 listen "/var/run/unicorn/hoot/socket"
@@ -11,4 +10,4 @@ pid "/var/run/unicorn/hoot/pid"
 stderr_path "/var/log/unicorn/hoot/error.log"
 stdout_path "/var/log/unicorn/hoot/access.log"
 
-user www-data www-data
+user("www-data", "www-data")
